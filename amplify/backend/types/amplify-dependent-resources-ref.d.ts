fd6c5a983f257a6516e3daaae106040fbf2fb051
adan73 <167682529+adan73@users.noreply.github.com>
2025-06-29 12:45:33 +0300

    Configure Cognito

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,8 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "wildrydes030a3e31": {
+    "wildrydesd27aec31": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
